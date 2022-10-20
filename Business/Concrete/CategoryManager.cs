6021e133cbf082e76d0155361e1210c17efff409
example <ahmte@DESKTOP-SO94CAG>
2022-10-20 14:31:51 +0300

    Add project files.

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,23 @@
+﻿using Data.Concrete.EntityFramework.Repositories;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> categoryRepo = new GenericRepository<Category>();
+        public List<Category> GetAllBl()
+        {
+            return categoryRepo.List();
+        }
+        public void CategoryAddBl(Category category)
+        {
+            categoryRepo.Insert(category);
+        }
+    }
+}
